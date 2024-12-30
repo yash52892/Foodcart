3bcd53657d865f73249a278456694e2bc04bd3ee
Yashwanth kumar R <97838432+yash52892@users.noreply.github.com>
2024-12-30 20:50:21 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,18 +4,18 @@ import Admin from './Admin/Admin';
 import Add from './Add/Add';
 import Login from './Auth/Login';
 import Signup from './Auth/Signup';
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 
 
 function App() {
-
+  const tok = localStorage.getItem("Token");
   return (
     <Routes>
       <Route path='/' element={<Auth/>}/>
       <Route path="/login" element={<Login/>}/>
       <Route path="/signup" element={<Signup/>}/>
-      <Route path="/admin" element={<Admin/>}/>
-      <Route path="/admin/addcatagories" element={<Add/>}/>
+      <Route path="/admin" element={tok ? (<Admin/>): (<Navigate to="/login" />)}/>
+      <Route path="/admin/addcatagories" element={tok ? (<Add/>): (<Navigate to="/login" />)}/>
     </Routes>
   );
 }
